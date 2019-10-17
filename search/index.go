7edf0aee7b90e568556715ffff05800deef85d0d
SigmaTsing <xushangqingxsq@126.com>
2019-10-17 14:46:57 +0800

    refactor(index): changed index init functions into independent packages

--- search/index.go
+++ search/index.go
@@ -1,14 +1,14 @@
 package search
 
 import(
-	"database/sql"
+	// "database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"backend/utils"
 	"strings"
-	"fmt"
+	// "fmt"
 	jsoniter "github.com/json-iterator/go"
 	// "backend/database"
-	"os"
+	// "os"
 	"io/ioutil"
 )
 
@@ -16,44 +16,7 @@ type IndexStruct struct {
 	Size int `json:"size"`
 	Gifs []utils.Gifs `json:"gifs"`
 }
-//生成json格式的index，由于速度问题暂时未采用
-func IndexInit(DB *sql.DB){
-	rows, err := DB.Query("Select Name,Title,Keyword from GIF_INFO")
-	defer func() {
-		if rows!=nil{
-			rows.Close();
-		}
-	}()
 
-	if err!=nil{
-		panic(err);
-	}
-
-	gif := new(utils.Gifs)
-	var ans []utils.Gifs
-
-	for rows.Next() {
-		if serr := rows.Scan(&gif.Name,&gif.Title,&gif.Keyword); serr != nil {
-			fmt.Printf("scan failed, err:%v\n", serr)
-			return
-		}
-		ans = append(ans, *gif)
-	}
-
-	var indexStruct IndexStruct
-	indexStruct.Size=len(ans)
-	indexStruct.Gifs=ans
-	// fmt.Println(indexStruct)
-	b, merr:=jsoniter.Marshal(indexStruct)
-	if merr!=nil{
-		fmt.Println(merr)
-		panic(merr)
-	}
-	w1,_ := os.OpenFile("searchIndex.json",os.O_CREATE|os.O_TRUNC,0644)
-	_,_= w1.Write(b)
-	_=w1.Close()
-	fmt.Println("Index Generated")
-}
 //返回json格式的index存储的Gif列表
 func IndexParse() []utils.Gifs{
 	bytes, _ := ioutil.ReadFile("searchIndex.json")
@@ -62,46 +25,6 @@ func IndexParse() []utils.Gifs{
 
 	return indexStruct.Gifs
 }
-//生成.ind格式的Gif列表
-func FastIndexInit(DB *sql.DB) {
-	rows, err := DB.Query("Select Name,Title,Keyword from GIF_INFO")
-	defer func() {
-		if rows!=nil{
-			rows.Close();
-		}
-	}()
-
-	if err!=nil{
-		panic(err);
-	}
-
-	var names []string
-	var titles []string
-	var keywords []string
-	var name,title,keyword string
-
-	for rows.Next() {
-		if serr := rows.Scan(&name,&title,&keyword); serr != nil {
-			fmt.Printf("scan failed, err:%v\n", serr)
-			return
-		}
-		names=append(names,name)
-		titles=append(titles,title)
-		keywords=append(keywords,keyword)
-	}
-
-	w1, _:=os.OpenFile("ind_name.ind",os.O_CREATE|os.O_TRUNC,0644)
-	_, _=w1.Write([]byte(strings.Join(names, "#")))
-	_=w1.Close()
-	// cache.FastWrite("ind_name.ind",[]byte(strings.Join(names, "#")))
-	w1, _=os.OpenFile("ind_title.ind",os.O_CREATE|os.O_TRUNC,0644)
-	_, _=w1.Write([]byte(strings.Join(titles, "#")))
-	_=w1.Close()
-	w1, _=os.OpenFile("ind_keyword.ind",os.O_CREATE|os.O_TRUNC,0644)
-	_, _=w1.Write([]byte(strings.Join(keywords, "#")))
-	_=w1.Close()
-	fmt.Println("FastIndex Inited")
-} 
 
 //读取.ind格式的index,下同
 func NameIndex() []string{
