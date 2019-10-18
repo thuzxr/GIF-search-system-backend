6660de8ed9c11ec5ec834c3fc8af5887731ecd24
wzq016 <wzq016@gmail.com>
2019-10-18 14:07:01 +0800

    refractor(datalayer): delete tf(draft)

--- tensorflow/python_models/func.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-def cosine(seq1, seq2):
-    norm1 = tf.norm(seq1 + 1e-5, axis=1, keepdims=True)
-    norm2 = tf.norm(seq2 + 1e-5, axis=1, keepdims=True)
-    sim = tf.matmul(seq1 / norm1, tf.transpose(seq2 / norm2, [1, 0]),name='cossim')
-    return sim
