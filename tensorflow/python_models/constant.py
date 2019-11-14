fbb26bc0606f4e78c1faef8c93af8aeb0e9a5394
wzq016 <wzq016@gmail.com>
2019-11-14 16:26:23 +0800

    feat(Recommend): add BERT to the recommend system #39

--- tensorflow/python_models/constant.py
+++
@@ -1,4 +0,0 @@
-embedding_file = "./data/embedding/sgns.wiki.bigram-char"
-embedding_dim = 300
-
-word2idx_file = "./data/word2idx.json"
--- tensorflow/python_models/models.py
+++ tensorflow/python_models/models.py
@@ -1,19 +1,21 @@
 import tensorflow as tf
-from python_models.func import cosine
+from func import cosine,cosine_bert
 import numpy as np
-import python_models.constant as constant
+from bert import modeling
 
 class CBOW():
-    def __init__(self,wordmat):
+    def __init__(self,wordmat,config):
+        self.config = config
         self.wordmat = wordmat
-        self.make_graph()
-        self.init()
-    def make_graph(self,scope='CBOW'):
+        self.model()
+        self.session()
+    def model(self,scope='CBOW'):
+        config = self.config
         with tf.variable_scope(scope,reuse = tf.AUTO_REUSE):
             with tf.variable_scope('Init'):
                 wordmat = tf.concat([
-                    tf.zeros([1, constant.embedding_dim], tf.float32, name='pad'),
-                    tf.get_variable('unk', shape=[1, constant.embedding_dim], dtype=tf.float32,
+                    tf.zeros([1, config.embedding_dim], tf.float32, name='pad'),
+                    tf.get_variable('unk', shape=[1, config.embedding_dim], dtype=tf.float32,
                                     initializer=tf.contrib.layers.xavier_initializer())
                     , tf.get_variable('wordmat', initializer=tf.constant(self.wordmat, dtype=tf.float32))], axis=0)
             with tf.variable_scope('placeholder'):
@@ -32,10 +34,66 @@ class CBOW():
             with tf.variable_scope('Sim'):
                 self.sim = cosine(emb1,emb2)-2*(1-tag_mask)
                 self.mean_sim = tf.div(tf.reduce_sum(tf.reduce_max(self.sim,axis=2),axis=1),numtag1,name='mean_sim')
-    def init(self):
+    def session(self):
         self.sess = sess = tf.Session()
         sess.run(tf.global_variables_initializer())
     def dump(self):
         builder = tf.saved_model.builder.SavedModelBuilder("CBOW")
         builder.add_meta_graph_and_variables(self.sess, ["var"])
         builder.save()
+
+class BERT():
+    def __init__(self,config):
+        self.config = config
+        self.model()
+        self.session()
+    def model(self):
+        config = self.config
+        bert_config = modeling.BertConfig.from_json_file(config.BERT_DIR+'/bert_config.json')
+
+        self.bert_input_ids = tf.placeholder (shape=[None,config.bert_max_len],dtype=tf.int32,name="input_ids")
+        self.bert_input_mask = tf.placeholder (shape=[None,config.bert_max_len],dtype=tf.int32,name="input_masks")
+        self.bert_segment_ids = tf.placeholder (shape=[None,config.bert_max_len],dtype=tf.int32,name="segment_ids")
+        self.bert_len = tf.placeholder(shape=[None],dtype=tf.int32,name='bert_len')
+        bert_model = modeling.BertModel(
+            config=bert_config,
+            is_training= False,
+            input_ids=self.bert_input_ids,
+            input_mask=self.bert_input_mask,
+            token_type_ids=self.bert_segment_ids,
+            use_one_hot_embeddings=False
+        )
+
+        self.bert_input_ids2 = tf.placeholder(shape=[None,config.bert_max_len],dtype=tf.int32,name="input_ids2")
+        self.bert_input_mask2 = tf.placeholder (shape=[None,config.bert_max_len],dtype=tf.int32,name="input_masks2")
+        self.bert_segment_ids2 = tf.placeholder (shape=[None,config.bert_max_len],dtype=tf.int32,name="segment_ids2")
+        self.bert_len2 = tf.placeholder(shape=[None],dtype=tf.int32,name='bert_len2')
+        bert_model2 = modeling.BertModel(
+            config=bert_config,
+            is_training= False,
+            input_ids=self.bert_input_ids2,
+            input_mask=self.bert_input_mask2,
+            token_type_ids=self.bert_segment_ids2,
+            use_one_hot_embeddings=False
+        )
+
+        self.numtags1 = tf.placeholder(shape=[1],dtype=tf.int32,name="numtag1")
+        self.numtags2 = tf.placeholder(shape=[1],dtype=tf.int32,name="numtag2")
+
+        tag_mask1 = tf.expand_dims(tf.reshape(tf.sequence_mask(self.numtags1,tf.shape(self.bert_input_ids)[0],dtype=tf.float32),[-1]),axis=1)
+        tag_mask2 = tf.expand_dims(tf.reshape(tf.sequence_mask(self.numtags2,tf.shape(self.bert_input_ids)[0],dtype=tf.float32),[-1]),axis=1)
+
+        tvars = tf.trainable_variables()
+
+        init_checkpoint = config.BERT_DIR+'/bert_model.ckpt'
+        (assignment_map, initialized_variable_names) = modeling.get_assignment_map_from_checkpoint(tvars,init_checkpoint)
+        tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+
+        output = bert_model.get_pooled_output()*tag_mask1
+        output2 = bert_model2.get_pooled_output()*tag_mask2
+
+        sim = cosine_bert(output,output2)
+        self.mean_sim = tf.div(tf.reduce_sum(tf.reduce_max(sim,axis=1),axis=0),tf.cast(self.numtags1[0],tf.float32),name='mean_sim')
+    def session(self):
+        self.sess = sess = tf.Session()
+        sess.run(tf.global_variables_initializer())
